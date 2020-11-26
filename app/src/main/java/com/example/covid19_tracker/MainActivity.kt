4a33f10f4dbaa26e7fb34a4b0a3c6f19449327f1
Your Name <you@example.com>
2020-11-26 18:02:40 +0530

    covid tracker

---
+++ app/src/main/java/com/example/covid19_tracker/MainActivity.kt
@@ -0,0 +1,81 @@
+package com.example.covid19_tracker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import com.google.gson.Gson
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.text.SimpleDateFormat
+import java.util.*
+import java.util.concurrent.TimeUnit
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var stateAdapter: StateAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        list.addHeaderView(LayoutInflater.from(this).inflate(R.layout.item_header,list,false))
+        fetchResult();
+    }
+
+    private fun fetchResult() {
+        GlobalScope.launch {
+            val response = withContext(Dispatchers.IO) { Client.api.execute() }
+            if (response.isSuccessful) {
+                val data = Gson().fromJson(response.body?.string(), Response::class.java)
+                launch(Dispatchers.Main) {
+                    bindCombinedData(data.statewise[0])
+                    bindStateWiseData(data.statewise.subList(0,data.statewise.size))
+                }
+            }
+        }
+    }
+
+    private fun bindStateWiseData(subList: List<Statewiseitem>) {
+
+        stateAdapter = StateAdapter((subList))
+        list.adapter =stateAdapter
+    }
+
+    private fun bindCombinedData(data: Statewiseitem) {
+
+        val lastUpdatedTime = data.lastupdatedtime
+        val simpleDateFormat = SimpleDateFormat("dd/MM/yyyy HH:mm:ss")
+        lastUpdatedTv.text = "Last Updated\n ${getTimeAgo(
+            simpleDateFormat.parse(lastUpdatedTime)
+        )}"
+        confirmedTv.text=data.confirmed
+        recoveredTv.text=data.recovered
+        activeTv.text=data.active
+        deceasedTv.text=data.deaths
+    }
+
+    fun getTimeAgo(past: Date): String {
+        val now = Date()
+        val seconds = TimeUnit.MILLISECONDS.toSeconds(now.time - past.time)
+        val minutes = TimeUnit.MILLISECONDS.toMinutes(now.time - past.time)
+        val hours = TimeUnit.MILLISECONDS.toHours(now.time - past.time)
+
+        return when {
+            seconds < 60 -> {
+                "Few seconds ago"
+            }
+            minutes < 60 -> {
+                "$minutes minutes ago"
+            }
+            hours < 24 -> {
+                "$hours hour ${minutes % 60} min ago"
+            }
+            else -> {
+                SimpleDateFormat("dd/MM/yy, hh:mm a").format(past).toString()
+            }
+        }
+    }
+}
